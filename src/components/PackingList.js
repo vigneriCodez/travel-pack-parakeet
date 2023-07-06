6e6f9dfe1d24eaa5d3b110870d433e5c6b65d6a3
Giancarlo Vigneri <vignericodes@gmail.com>
2023-07-06 16:08:14 -0700

    add sort feature to list

--- src/components/PackingList.js
+++ src/components/PackingList.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Item from './Item';
 
 function PackingList({ items, onDeleteItem, onToggleItem }) {
+	const [sortBy, setSortBy] = useState('input');
+
+	let sortedItems;
+
+	if (sortBy === 'input') sortedItems = items;
+
+	if (sortBy === 'description')
+		sortedItems = items
+			.slice()
+			.sort((a, b) => a.description.localeCompare(b.description));
+
+	if (sortBy === 'packed')
+		sortedItems = items
+			.slice()
+			.sort((a, b) => Number(a.packed) - Number(b.packed));
+
 	return (
 		<div className='list'>
 			<ul>
-				{items.map((item) => {
+				{sortedItems.map((item) => {
 					return (
 						<Item
 							key={item.id}
@@ -17,6 +33,13 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
 					);
 				})}
 			</ul>
+			<div className='actions'>
+				<select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+					<option value='input'>Sort by input order</option>
+					<option value='description'>Sort by description</option>
+					<option value='packed'>Sort by packed status</option>
+				</select>
+			</div>
 		</div>
 	);
 }
